ea0fc971a0e007f1c5304c48af26a9dcf8e9b4c0
Mark Brooks <Markabrooks@gmail.com>
2024-02-11 13:43:00 -0700

    add auth

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import './App.css';
+import './App.css'
+import { Amplify } from 'aws-amplify'
+import awsConfig from './aws-exports'
+import { Authenticator, signOut } from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css'
 
-function App() {
+Amplify.configure(awsConfig)
+
+export default function App() {
   return (
-    <div className="App">
-      <h1> Hello World </h1>
-    </div>
-  );
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
+  )
 }
 
-export default App;
+
+
